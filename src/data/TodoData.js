bd0b20a363831b1c6e5b428a29e9f2831d93762f
Taofiq <animashauntaofiq@gmail.com>
2025-01-01 20:35:55 +0100

    Add TodoData module with initial todo items for management

---
+++ src/data/TodoData.js
@@ -0,0 +1,29 @@
+const TodoData = [
+  {
+    id: 1,
+    title: "Go to the market",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "Buy food",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Cook food",
+    completed: false
+  },
+  {
+    id: 4,
+    title: "Eat food",
+    completed: false
+  },
+  {
+    id: 5,
+    title: "Sleep",
+    completed: false
+  }
+]
+
+export default  TodoData;
