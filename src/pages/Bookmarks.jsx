b1125e2c354a9d5934bd6061ccd619134caae87f
Taofiq <animashauntaofiq@gmail.com>
2025-02-25 00:51:13 +0100

    Add Bookmarks page with search functionality for bookmarked shows

---
+++ src/pages/Bookmarks.jsx
@@ -0,0 +1,9 @@
+import Search from "../components/Ui/Input/Search";
+
+export default function Bookmarks() {
+  return (
+    <div>
+      <Search placeholder={"Search for bookmarked shows"} />
+    </div>
+  );
+}
