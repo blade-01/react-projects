d2bc06e1c9fdc8f10b9783cc42269431e4d9614a
Taofiq <animashauntaofiq@gmail.com>
2025-02-25 00:51:25 +0100

    Add Series page with Search component for TV series queries

---
+++ src/pages/Series.jsx
@@ -0,0 +1,9 @@
+import Search from "../components/Ui/Input/Search";
+
+export default function Series() {
+  return (
+    <div>
+      <Search placeholder={"Search for TV series"} />
+    </div>
+  );
+}
