d0cff1034bf2c5881ad3519ed6e4f9ad845a4bf7
Taofiq <animashauntaofiq@gmail.com>
2025-02-25 22:57:33 +0100

    Wrap application in PrimeReactProvider and add Search, MovieDetail, and SeriesDetail routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { PrimeReactProvider } from "primereact/api";
 import Sidebar from "./components/Navigation/Sidebar";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Series from "./pages/Series";
 import Bookmarks from "./pages/Bookmarks";
+import Search from "./pages/Search";
+import MovieDetail from "./pages/MovieDetail";
+import SeriesDetail from "./pages/SeriesDetail";
 
 function App() {
   return (
-    <Router>
-      <Sidebar />
-      <div className="md:ml-30 md:mt-10 p-4 2xl:mx-auto max-w-[1500px]">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/series" element={<Series />} />
-          <Route path="/bookmarks" element={<Bookmarks />} />
-        </Routes>
-      </div>
-    </Router>
+    <PrimeReactProvider>
+      <Router>
+        <Sidebar />
+        <div className="xl:ml-30 xl:mt-8 2xl:mt-10 p-4 2xl:mx-auto max-w-[1500px]">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/bookmarks" element={<Bookmarks />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/movies/:id" element={<MovieDetail />} />
+            <Route path="/series/:id" element={<SeriesDetail />} />
+          </Routes>
+        </div>
+      </Router>
+    </PrimeReactProvider>
   );
 }
 
