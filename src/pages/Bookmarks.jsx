47d0470b67ba23c8ce2bb1b1e41063455075d933
Taofiq <animashauntaofiq@gmail.com>
2025-02-27 00:20:04 +0100

    Add id property to Card component in Bookmarks page

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -15,6 +15,7 @@ export default function Bookmarks() {
             <Card
               key={item}
               data={{
+                id: "1234",
                 poster:
                   "https://image.tmdb.org/t/p/original//xuLA0pii2IMJW2puT7EvJtgpg0H.jpg",
                 title: "Sonic Hedgehog",
