760cb02487437f2521bdd8fe3b1e9acbb8bbee48
Taofiq <animashauntaofiq@gmail.com>
2025-04-04 00:29:41 +0100

    Refactor MovieDetail component to fetch movie data from API and display loading/error states

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,109 +1,50 @@
 import { useParams } from "react-router";
 import Btn from "../components/Ui/Btn";
-import { useEffect, useState } from "react";
 import Details from "../components/Media/Details";
+import Search from "../components/Ui/Input/Search";
+import useFetch from "../hooks/useFetch";
 
 export default function MovieDetail() {
   const { id } = useParams();
-  const [data] = useState({
-    id: "1234",
-    poster:
-      "https://image.tmdb.org/t/p/original//6DrHO1jr3qVrViUO6s6kFiAGM7.jpg",
-    title: "Sonic the Hedgehog",
-    subTitle: "Welcome to the next level.",
-    synopsis:
-      "After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands.",
-    rating: 3.8,
-    year: 2025,
-    type: "movies",
-    genres: ["Action", "Adventure", "Family", "Comedy"],
-    casts: [
-      "Ben Schwartz",
-      "James Marsden",
-      "Tika Sumpter",
-      "Colleen O'Shaughnessey",
-      "Idris Elba",
-      "Jim Carrey",
-      "Natasha Rothwell",
-      "Adam Pally",
-      "Shemar Moore",
-      "Lee Majdoub",
-      "Melody Nosipho Niemann",
-      "Tom Butler",
-      "Brad Kalilimoku",
-      "Krista Alvarez",
-      "Donna Jay Fulks",
-      "Scott Patey",
-      "Leif Havdale",
-      "Johnson Phan",
-      "Colby Chartrand",
-      "Kevin Fortin",
-      "Jeff Sanca",
-      "Sook Hexamer",
-      "Maria Ameerali",
-      "Kyle Riefsnyder",
-      "Parker Rowell-Laferriere",
-      "Tammy Nera",
-      "Gerald Paetz",
-      "Corry Glass",
-      "David Jacox",
-      "Mike Mitchell",
-      "Heath Stevenson",
-      "Doug Chapman",
-      "Rhys Williams",
-      "Nilo Ghajar",
-      "Don Lew",
-      "Elizabeth Bowen",
-      "Vladimir Ruzich",
-      "Shay Kuebler",
-      "Keiran Bohay",
-      "Kevin Mylrea",
-      "Jared Khalifa",
-      "Aiden Cass",
-      "Andrew Kyrzyk",
-      "Stanislav Galimkhanov",
-      "Shaun Magee",
-      "Alex Bogomolov",
-      "Shawn Stewart",
-      "Barry Nerling",
-      "Simon Chin",
-      "Vladimir Raiman",
-      "Yvetta Fisher",
-      "Adrian Hein",
-      "Jess Lundgren",
-      "Steve Chang",
-      "Ted Barba",
-      "Brennan Dyson",
-      "Saida Dyson",
-      "Katie Wright Pere",
-      "Ha'a Keaulana",
-      "Quinn Early",
-      "Cheryl Lewis",
-      "Angela Meryl",
-      "Ashlei Tave",
-      "Mariah Dyson",
-      "Tavita Woodard",
-      "Ernie Jackson",
-      "Rob 'Sluggo' Boyce",
-      "Robert Zen Humpage",
-      "Fraser Corbett",
-      "Marcus Aurelio",
-      "Jason Triplett",
-      "Eli Olson",
-      "Mike Rufino",
-      "Nito Larioza",
-      "Chad Keaulana",
-      "Sarah Surh",
-      "Paul Lazenby"
-    ]
-  });
-  useEffect(() => {
-    console.log("mounted", id);
-  }, [id]);
+  const { data, loading, error } = useFetch(
+    `/movie/${id}?append_to_response=casts,videos&language=en-US`
+  );
+
+  // Display loading state or error
+  if (loading) return <p>Loading movie details...</p>;
+  if (error) return <p>Something went wrong! {error.message}</p>;
+
   return (
     <div>
+      <Search placeholder={"Search for movies"} />
       <Btn />
-      <Details data={data} />
+
+      {/* Only render Details if data is available */}
+      {data && (
+        <Details
+          data={{
+            id: data.id,
+            poster: data.poster_path
+              ? `https://image.tmdb.org/t/p/original/${data.poster_path || ""}`
+              : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+            title: data.original_title || "N/A",
+            subTitle: data.tagline,
+            synopsis: data.overview || "No synopsis available",
+            rating: data.vote_average || 0,
+            year: new Date(data.release_date).getFullYear() || "N/A",
+            genres: data.genres || [],
+            length: data.runtime || "N/A",
+            language: data.spoken_languages?.[0]?.name || "N/A",
+            status: data.status || "N/A",
+            casts: data.casts?.cast || [],
+            link: {
+              website: data.homepage,
+              imdb: data.imdb_id,
+              youtube: data.videos?.results[0]?.key
+            }
+          }}
+        />
+      )}
     </div>
   );
 }
