1915e771ac94a5c2954342aeec5600091fcc7200
Taofiq <animashauntaofiq@gmail.com>
2025-04-04 00:27:59 +0100

    Refactor routing in App component to include Movie and Tv pages with dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { PrimeReactProvider } from "primereact/api";
 import Sidebar from "./components/Navigation/Sidebar";
 import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import Series from "./pages/Series";
+import Movie from "./pages/Movie";
+import Tv from "./pages/Tv";
 import Bookmarks from "./pages/Bookmarks";
 import Search from "./pages/Search";
 import MovieDetail from "./pages/MovieDetail";
-import SeriesDetail from "./pages/SeriesDetail";
+import MovieList from "./pages/MovieList";
+import TvList from "./pages/TvList";
+import TvDetail from "./pages/TvDetail";
 
 function App() {
   return (
@@ -17,12 +19,14 @@ function App() {
         <div className="xl:ml-30 xl:mt-8 2xl:mt-10 p-4 2xl:mx-auto max-w-[1500px]">
           <Routes>
             <Route index element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/series" element={<Series />} />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/tv" element={<Tv />} />
             <Route path="/bookmarks" element={<Bookmarks />} />
             <Route path="/search" element={<Search />} />
-            <Route path="/movies/:id" element={<MovieDetail />} />
-            <Route path="/series/:id" element={<SeriesDetail />} />
+            <Route path="/movie/:id" element={<MovieDetail />} />
+            <Route path="/movie/:type/list" element={<MovieList />} />
+            <Route path="/tv/:id" element={<TvDetail />} />
+            <Route path="/tv/:type/list" element={<TvList />} />
           </Routes>
         </div>
       </Router>
