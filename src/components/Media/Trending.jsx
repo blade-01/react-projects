7f5cb33062b62a599ff4c2c59e4f3a686b1411d9
Taofiq <animashauntaofiq@gmail.com>
2025-04-04 00:28:55 +0100

    Implement navigation for Trending component and update year formatting

--- src/components/Media/Trending.jsx
+++ src/components/Media/Trending.jsx
@@ -3,17 +3,24 @@ import { IoBookmarkOutline } from "react-icons/io5";
 import { TbDeviceTvOld } from "react-icons/tb";
 import { RiFilmFill } from "react-icons/ri";
 import { LuDot } from "react-icons/lu";
+import { useNavigate } from "react-router";
 
 export default function Trending({ data }) {
+  const navigateTo = useNavigate();
   return (
-    <div className="relative cursor-pointer">
+    <div
+      className="relative cursor-pointer"
+      onClick={() => {
+        navigateTo(`/${data.type}/${data.id}`);
+      }}
+    >
       <div className="bookmark-icon group">
         <IoBookmarkOutline className="text-white group-hover:text-black" />
       </div>
       <div className="relative overflow-hidden rounded-md">
         <div className="home-media--card  transition duration-300 transform hover:scale-105">
           <img
-            src="https://image.tmdb.org/t/p/original//xuLA0pii2IMJW2puT7EvJtgpg0H.jpg"
+            src={data.poster}
             alt="banner"
             className="w-full h-full rounded-md object-cover "
           />
@@ -21,7 +28,7 @@ export default function Trending({ data }) {
       </div>
       <div className="absolute bottom-2 left-2 md:bottom-3 md:left-3 z-10">
         <div className="flex gap-0.5 items-center text-sm text-gray-100 pb-0.5">
-          <p>{data.year}</p>
+          <p>{new Date(data.year).getFullYear()}</p>
           <LuDot />
           <div className="flex items-start gap-1">
             {data.type === "movie" ? (
@@ -31,8 +38,6 @@ export default function Trending({ data }) {
             )}
             <p>{data.type === "movie" ? "Movie" : "TV Series"}</p>
           </div>
-          <LuDot />
-          <p>18+</p>
         </div>
         <p className="text-xl md:text-2xl font-medium text-white ">
           {data.title}
@@ -44,6 +49,7 @@ export default function Trending({ data }) {
 
 Trending.propTypes = {
   data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     year: PropTypes.number.isRequired,
