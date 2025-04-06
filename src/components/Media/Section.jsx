38308a3719165c0b5ad438122e6dd55ff71e3172
Taofiq <animashauntaofiq@gmail.com>
2025-04-06 01:11:18 +0100

    Add pagination and search functionality to Section component

--- src/components/Media/Section.jsx
+++ src/components/Media/Section.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
-import { NavLink } from "react-router";
+import { NavLink, useSearchParams, useLocation } from "react-router";
 import Card from "./Card";
 import Trending from "./Trending";
+import { Paginator } from "primereact/paginator";
+import { useEffect, useState } from "react";
 
 export default function Section({
   title,
@@ -10,77 +12,131 @@ export default function Section({
   type,
   isTrending = false,
   isPaginated = false,
-  link
+  link,
+  setPage
 }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [first, setFirst] = useState(0);
+
+  const onPageChange = (event) => {
+    setFirst(event.first);
+    searchParams.set("page", event.page + 1);
+    setSearchParams(searchParams);
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (
+      (location.pathname === "/tv" || location.pathname === "/movie") &&
+      (searchParams.get("page") === null ||
+        searchParams.get("page") === undefined)
+    ) {
+      searchParams.set("page", 1);
+      setSearchParams(searchParams);
+      return;
+    }
+
+    if (searchParams.get("page")) {
+      const pageNum = parseInt(searchParams.get("page"));
+      setFirst((pageNum - 1) * 10);
+      setPage(pageNum);
+    }
+  }, [searchParams]);
+
   return (
-    <div className="mb-10">
-      <div className="flex justify-between items-center mb-5">
-        <div className="flex items-center gap-1.5">
-          <h2 className="text-white font-normal text-xl md:text-3xl capitalize">
-            {title}
-          </h2>
-          {!isPaginated && (
-            <p
-              className={`text-xs uppercase text-white border border-white rounded-md font-semibold px-1.5 mt-1.5 ${
-                type === "tv" ? "bg-white !text-main-bg " : "bg-transparent"
-              }`}
-            >
-              {type}
-            </p>
+    <>
+      <div className="mb-10">
+        <div className="flex justify-between items-center mb-5">
+          <div className="flex items-center gap-1.5">
+            <h2 className="text-white font-normal text-xl md:text-3xl capitalize">
+              {title}
+            </h2>
+            {!isPaginated && (
+              <p
+                className={`text-xs uppercase text-white border border-white rounded-md font-semibold px-1.5 mt-1.5 ${
+                  type === "tv" ? "bg-white !text-main-bg " : "bg-transparent"
+                }`}
+              >
+                {type}
+              </p>
+            )}
+          </div>
+          {link && (
+            <NavLink to={link} className="uppercase font-semibold text-sm">
+              See more
+            </NavLink>
           )}
         </div>
-        {link && (
-          <NavLink to={link} className="uppercase font-semibold text-sm">
-            See more
-          </NavLink>
+        {loading ? (
+          <p>Loading {title.toLowerCase()}...</p>
+        ) : (
+          <div
+            className={`${
+              isTrending
+                ? "flex gap-5 md:gap-7 w-full overflow-auto no-scrollbar relative"
+                : "grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-7"
+            }`}
+          >
+            {data?.results
+              ?.slice(0, isPaginated ? data.results.length : 8)
+              .map((item) =>
+                isTrending ? (
+                  <Trending
+                    key={item.id}
+                    data={{
+                      id: item.id,
+                      poster: `https://image.tmdb.org/t/p/original/${item.backdrop_path}`,
+                      title: item.original_title || item.name,
+                      year:
+                        type === "movie"
+                          ? item.release_date
+                          : item.first_air_date,
+                      type
+                    }}
+                  />
+                ) : (
+                  <Card
+                    key={item.id}
+                    data={{
+                      id: item.id,
+                      poster: item.backdrop_path
+                        ? `https://image.tmdb.org/t/p/original/${
+                            item.backdrop_path || ""
+                          }`
+                        : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+                      title: item.original_title || item.name,
+                      year:
+                        type === "movie"
+                          ? item.release_date
+                          : item.first_air_date,
+                      type
+                    }}
+                  />
+                )
+              )}
+          </div>
         )}
       </div>
-      {loading ? (
-        <p>Loading {title.toLowerCase()}...</p>
-      ) : (
-        <div
-          className={`${
-            isTrending
-              ? "flex gap-5 md:gap-7 w-full overflow-auto no-scrollbar relative"
-              : "grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-7"
-          }`}
-        >
-          {data?.results
-            ?.slice(0, isPaginated ? data.results.length : 8)
-            .map((item) =>
-              isTrending ? (
-                <Trending
-                  key={item.id}
-                  data={{
-                    id: item.id,
-                    poster: `https://image.tmdb.org/t/p/original/${item.backdrop_path}`,
-                    title: item.original_title || item.name,
-                    year:
-                      type === "movie"
-                        ? item.release_date
-                        : item.first_air_date,
-                    type
-                  }}
-                />
-              ) : (
-                <Card
-                  key={item.id}
-                  data={{
-                    id: item.id,
-                    poster: `https://image.tmdb.org/t/p/original/${item.backdrop_path}`,
-                    title: item.original_title || item.name,
-                    year:
-                      type === "movie"
-                        ? item.release_date
-                        : item.first_air_date,
-                    type
-                  }}
-                />
-              )
-            )}
+      {isPaginated && !loading && (
+        <div className="flex justify-center my-5">
+          <Paginator
+            first={first}
+            rows={10}
+            totalRecords={data?.total_pages}
+            onPageChange={onPageChange}
+            template={{ layout: "PrevPageLink CurrentPageReport NextPageLink" }}
+            pt={{
+              root: {
+                className:
+                  "!bg-sidebar-bg !inline-flex !shadow-md !border-main-bg"
+              }
+            }}
+          />
         </div>
       )}
-    </div>
+    </>
   );
 }
 
@@ -91,5 +147,6 @@ Section.propTypes = {
   type: PropTypes.oneOf(["movie", "tv"]).isRequired,
   isTrending: PropTypes.bool,
   isPaginated: PropTypes.bool,
-  link: PropTypes.string
+  link: PropTypes.string,
+  setPage: PropTypes.func
 };
