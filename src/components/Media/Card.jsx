3cdad42df426cb8f4cadcc28538650f5e53c3a16
Taofiq <animashauntaofiq@gmail.com>
2025-04-09 15:38:19 +0100

    Enhance Card component with loading and bookmarked states; integrate lazy loading for images

--- src/components/Media/Card.jsx
+++ src/components/Media/Card.jsx
@@ -1,12 +1,15 @@
 import PropTypes from "prop-types";
-import { IoBookmarkOutline } from "react-icons/io5";
+import { IoBookmarkOutline, IoBookmark } from "react-icons/io5";
 import { TbDeviceTvOld } from "react-icons/tb";
 import { RiFilmFill } from "react-icons/ri";
 import { LuDot } from "react-icons/lu";
+import { TbLoader } from "react-icons/tb";
 import { motion } from "motion/react";
 import { useNavigate } from "react-router";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-export default function Card({ data, openModal }) {
+export default function Card({ data, openModal, loading, bookmarked }) {
   const navigateTo = useNavigate();
   return (
     <>
@@ -29,12 +32,21 @@ export default function Card({ data, openModal }) {
             openModal();
           }}
         >
-          <IoBookmarkOutline className="text-white group-hover:text-black" />
+          {loading ? (
+            <TbLoader className="animate-spin" />
+          ) : bookmarked ? (
+            <IoBookmark className="text-white group-hover:text-black" />
+          ) : (
+            <IoBookmarkOutline className="text-white group-hover:text-black" />
+          )}
         </div>
-        <img
+        <LazyLoadImage
           src={data.poster}
           alt={data.title}
+          effect="blur"
+          placeholderSrc="https://miro.medium.com/v2/resize:fit:832/format:webp/1*wyI7Pb_WLjVry95xLx93eg.gif"
           className="h-[110px] md:h-[140px] xl:h-[174px] w-full object-cover rounded-lg mb-2"
+          wrapperClassName="h-[110px] md:h-[140px] xl:h-[174px] w-full mb-2 rounded-lg overflow-hidden"
         />
         <div className="p-0.5">
           <div className="flex gap-0.5 items-center text-xs text-gray-100 pb-0.5">
@@ -64,5 +76,7 @@ Card.propTypes = {
     year: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired
   }).isRequired,
-  openModal: PropTypes.func
+  openModal: PropTypes.func,
+  loading: PropTypes.bool,
+  bookmarked: PropTypes.bool
 };
