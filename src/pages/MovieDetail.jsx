f1cb2de0b519a0820475c621e4ce81857b5c63d7
Taofiq <animashauntaofiq@gmail.com>
2025-04-09 15:38:59 +0100

    Replace loading message with Loader component in MovieDetail for improved user experience

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -3,6 +3,7 @@ import Btn from "../components/Ui/Btn";
 import Details from "../components/Media/Details";
 import Search from "../components/Ui/Input/Search";
 import useFetch from "../hooks/useFetch";
+import Loader from "../components/Ui/Loader";
 
 export default function MovieDetail() {
   const { id } = useParams();
@@ -11,7 +12,7 @@ export default function MovieDetail() {
   );
 
   // Display loading state or error
-  if (loading) return <p>Loading movie details...</p>;
+  if (loading) return <Loader height={"h-[80vh]"} />;
   if (error) return <p>Something went wrong! {error.message}</p>;
 
   return (
