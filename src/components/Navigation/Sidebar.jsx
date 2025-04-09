fd4acff09706306ea4f51cfefc918d9a0b25253f
Taofiq <animashauntaofiq@gmail.com>
2025-04-09 15:38:42 +0100

    Implement user authentication in Sidebar component; display user avatar or default image

--- src/components/Navigation/Sidebar.jsx
+++ src/components/Navigation/Sidebar.jsx
@@ -4,8 +4,19 @@ import { RiFilmFill } from "react-icons/ri";
 import { BiSolidDashboard } from "react-icons/bi";
 import { IoMdBookmark } from "react-icons/io";
 import { NavLink } from "react-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
+  const [authUser, setAuthUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setAuthUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="bg-sidebar-bg text-sidebar-text sticky z-10 xl:fixed xl:left-5 xl:top-1/2 xl:-translate-y-1/2 xl:h-[94dvh]  xl:w-22 top-0 p-4 flex justify-between items-center xl:flex-col xl:justify-start xl:rounded-[20px] xl:py-6">
       <NavLink to="/">
@@ -26,11 +37,19 @@ export default function Sidebar() {
         </NavLink>
       </div>
       <div className="xl:mt-auto">
-        <img
-          src="https://avatars.githubusercontent.com/u/47092407?v=4"
-          alt="avatar"
-          className="w-7 h-6 rounded-full xl:w-8 xl:h-8 border border-white"
-        />
+        {authUser?.photoURL ? (
+          <img
+            src={authUser?.photoURL}
+            alt="avatar"
+            className="w-7 h-6 rounded-full xl:w-8 xl:h-8 border border-white"
+          />
+        ) : (
+          <img
+            src="https://freesvg.org/img/abstract-user-flat-4.png"
+            alt="avatar"
+            className="w-7 h-6 rounded-full xl:w-8 xl:h-8 border border-white"
+          />
+        )}
       </div>
     </div>
   );
