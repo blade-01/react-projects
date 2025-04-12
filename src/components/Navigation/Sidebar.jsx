794d2d9c5b908b4586d8dc22d9c1d239a54ab12a
Taofiq <animashauntaofiq@gmail.com>
2025-04-12 22:58:30 +0100

    Add authentication handling and modal to Sidebar component

--- src/components/Navigation/Sidebar.jsx
+++ src/components/Navigation/Sidebar.jsx
@@ -5,11 +5,17 @@ import { BiSolidDashboard } from "react-icons/bi";
 import { IoMdBookmark } from "react-icons/io";
 import { NavLink } from "react-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { Menu } from "primereact/menu";
+import AuthModal from "../Auth/Modal";
+import { useNavigate } from "react-router";
+import { HiOutlineLogin, HiOutlineLogout } from "react-icons/hi";
 
 export default function Sidebar() {
   const [authUser, setAuthUser] = useState(null);
+  const [visible, setVisible] = useState(false);
   const auth = getAuth();
+  const navigateTo = useNavigate();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setAuthUser(user);
@@ -17,6 +23,33 @@ export default function Sidebar() {
 
     return () => unsubscribe();
   }, []);
+
+  const menu = useRef(null);
+  const itemRenderer = (item) => (
+    <div className="p-menuitem-content" onClick={() => item.command()}>
+      <a className="flex items-center p-menuitem-link cursor-pointer">
+        {item.icon && <item.icon className="text-base" />}
+        <span className="mx-2">{item.label}</span>
+      </a>
+    </div>
+  );
+  const items = [
+    {
+      label: authUser ? "Logout" : "Login",
+      icon: authUser ? HiOutlineLogout : HiOutlineLogin,
+      template: itemRenderer,
+      command: () => {
+        if (authUser) {
+          auth.signOut();
+          setAuthUser(null);
+          navigateTo("/");
+          menu.current.toggle(false);
+        } else {
+          setVisible(true);
+        }
+      }
+    }
+  ];
   return (
     <div className="bg-sidebar-bg text-sidebar-text sticky z-10 xl:fixed xl:left-5 xl:top-1/2 xl:-translate-y-1/2 xl:h-[94dvh]  xl:w-22 top-0 p-4 flex justify-between items-center xl:flex-col xl:justify-start xl:rounded-[20px] xl:py-6">
       <NavLink to="/">
@@ -36,7 +69,11 @@ export default function Sidebar() {
           <IoMdBookmark size={24} />
         </NavLink>
       </div>
-      <div className="xl:mt-auto">
+      <div
+        className="xl:mt-auto"
+        aria-controls="popup_menu_left"
+        onClick={(event) => menu.current.toggle(event)}
+      >
         {authUser?.photoURL ? (
           <img
             src={authUser?.photoURL}
@@ -51,6 +88,8 @@ export default function Sidebar() {
           />
         )}
       </div>
+      <Menu model={items} popup ref={menu} id="popup_menu_left" />
+      <AuthModal visible={visible} setVisible={setVisible} />
     </div>
   );
 }
