9110aef332d906865735813c028fcd3dda5a3fce
Taofiq <animashauntaofiq@gmail.com>
2025-04-13 11:46:06 +0100

    Adjust padding in Search component for improved layout

--- src/components/Ui/Input/Search.jsx
+++ src/components/Ui/Input/Search.jsx
@@ -86,7 +86,7 @@ export default function Search({
 
   return (
     <div>
-      <div className="relative bg-transparent p-2 flex items-center gap-2 mb-4 basis-[80%]">
+      <div className="relative bg-transparent py-2 flex items-center gap-2 mb-4 basis-[80%]">
         <FiSearch className="text-white text-[25px] md:text-3xl" />
 
         <input
